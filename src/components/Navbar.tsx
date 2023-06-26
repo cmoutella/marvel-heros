8f8cddc5ee087484c9d765b05987baab92bb7c00
Carolina Moutella <ola@carolinamoutella.com>
2023-06-26 18:07:42 -0300

    feat(Navbar): create component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="bg-darker py-3 px-5 w-full drop-shadow-md fixed">
+      <Link href="/">Navbar</Link>
+    </div>
+  );
+};
+
+export default Navbar;
