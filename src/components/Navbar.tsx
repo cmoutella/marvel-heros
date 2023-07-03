61ee5e7d83684f2ac4670e51cb29ac9eade8a7e0
Carolina Moutella <ola@carolinamoutella.com>
2023-07-03 20:05:43 -0300

    fix(Navbar): add logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="bg-darker py-3 px-5 w-full drop-shadow-md fixed">
-      <Link href="/">Navbar</Link>
+    <div className="bg-darker py-3 px-5 w-full drop-shadow-md fixed flex justify-center">
+      <Link href="/">
+        <Image src={logo} alt="Marvel small logo" height={24} width={24} />
+      </Link>
     </div>
   );
 };
